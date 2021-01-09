2c890d38d26cd905d0d87da82f1d70e2bbd59e14
astolbin <astolbin@ya.ru>
2021-01-09 19:08:44 +0300

    1. Читаем файл конфигурации [#346045]

---
+++ chapter_002/src/main/java/file/Config.java
@@ -0,0 +1,46 @@
+package file;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+
+public class Config {
+    private final String path;
+    private final Map<String, String> values = new HashMap<>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().filter(line -> (!line.isEmpty() && !line.startsWith("#")))
+                    .map(line -> line.split("="))
+                    .filter(arr -> arr.length == 2)
+                    .forEach(arr -> values.put(arr[0], arr[1]));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        return values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner out = new StringJoiner(System.lineSeparator());
+        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+            read.lines().forEach(out::add);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return out.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Config("app.properties"));
+    }
+}
