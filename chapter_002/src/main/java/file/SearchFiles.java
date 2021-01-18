104326de26b9e3d61bde521123cd20cf2299880d
astolbin <astolbin@ya.ru>
2021-01-18 08:43:38 +0300

    4.1. Сканирование файловой системы. [#346036]

--- chapter_002/src/main/java/file/SearchFiles.java
+++ chapter_002/src/main/java/file/SearchFiles.java
@@ -2,8 +2,8 @@ package file;
 
 import java.io.IOException;
 import java.nio.file.FileVisitResult;
-import java.nio.file.FileVisitor;
 import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,17 +11,12 @@ import java.util.function.Predicate;
 
 import static java.nio.file.FileVisitResult.CONTINUE;
 
-public class SearchFiles implements FileVisitor<Path> {
-    Predicate<Path> predicate;
-    List<Path> paths = new ArrayList<>();
+public class SearchFiles extends SimpleFileVisitor<Path> {
+    private final Predicate<Path> predicate;
+    private final List<Path> paths = new ArrayList<>();
 
     public SearchFiles(Predicate<Path> predicate) {
         this.predicate = predicate;
-    }
-
-    @Override
-    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
-        return CONTINUE;
     }
 
     @Override
@@ -29,16 +24,6 @@ public class SearchFiles implements FileVisitor<Path> {
         if (predicate.test(file)) {
             paths.add(file);
         }
-        return CONTINUE;
-    }
-
-    @Override
-    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
-        return CONTINUE;
-    }
-
-    @Override
-    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
         return CONTINUE;
     }
 
