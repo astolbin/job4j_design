cad3dbf16b15771c363a8e14b4032472a88abbce
astolbin <astolbin@ya.ru>
2021-01-19 22:06:06 +0300

    5.1. Именованные аргументы [#346043]

---
+++ chapter_002/src/main/java/file/ArgsName.java
@@ -0,0 +1,37 @@
+package file;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArgsName {
+
+    private final Map<String, String> values = new HashMap<>();
+
+    public String get(String key) {
+        if (!values.containsKey(key)) {
+            throw new IllegalArgumentException("Argument not found");
+        }
+        return values.get(key);
+    }
+
+    private void parse(String[] args) {
+        Arrays.stream(args)
+                .map(param -> param.split("="))
+                .forEach(param -> values.put(param[0].substring(1), param[1]));
+    }
+
+    public static ArgsName of(String[] args) {
+        ArgsName names = new ArgsName();
+        names.parse(args);
+        return names;
+    }
+
+    public static void main(String[] args) {
+        ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512", "-encoding=UTF-8"});
+        System.out.println(jvm.get("Xmx"));
+
+        ArgsName zip = ArgsName.of(new String[] {"-out=project.zip", "-encoding=UTF-8"});
+        System.out.println(zip.get("out"));
+    }
+}
