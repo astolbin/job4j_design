cce6a94ceff1804d9a7380519f01a971b2db6441
astolbin <astolbin@ya.ru>
2021-01-19 21:46:48 +0300

    4.2. Поиск дубликатов [#346044]

--- chapter_002/src/main/java/file/SearchDuplicates.java
+++ chapter_002/src/main/java/file/SearchDuplicates.java
@@ -2,50 +2,29 @@ package file;
 
 import java.io.IOException;
 import java.nio.file.FileVisitResult;
-import java.nio.file.FileVisitor;
 import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.*;
-import java.util.stream.Collectors;
 
 import static java.nio.file.FileVisitResult.CONTINUE;
 
-public class SearchDuplicates implements FileVisitor<Path> {
-    private final Map<String, List<Path>> duplicates = new HashMap<>();
-
-    @Override
-    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
-        return CONTINUE;
-    }
+public class SearchDuplicates extends SimpleFileVisitor<Path> {
+    private final Set<String> uniqueFiles = new HashSet<>();
+    private final List<Path> duplicates = new ArrayList<>();
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        List<Path> paths;
-        String key = file.toFile().getName() + file.toFile().length();
-        if (duplicates.containsKey(key)) {
-            paths = duplicates.get(key);
+        String fileParams = file.toFile().getName() + file.toFile().length();
+        if (uniqueFiles.contains(fileParams)) {
+            duplicates.add(file);
         } else {
-            paths = new ArrayList<>();
-            duplicates.put(key, paths);
+            uniqueFiles.add(fileParams);
         }
-        paths.add(file);
-        return CONTINUE;
-    }
-
-    @Override
-    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
-        return CONTINUE;
-    }
-
-    @Override
-    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
         return CONTINUE;
     }
 
     public List<Path> getDuplicates() {
-        return duplicates.values().stream()
-                .filter(files -> files.size() > 1)
-                .flatMap(Collection::stream)
-                .collect(Collectors.toList());
+        return duplicates;
     }
 }
