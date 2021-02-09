270cf30b53ae4786d0a940072d366f07c73998e6
astolbin <astolbin@ya.ru>
2021-02-09 07:47:09 +0300

    3. Slf4j - вывод переменных. [#346033]

--- chapter_002/src/main/java/file/UsageLog4j.java
+++ chapter_002/src/main/java/file/UsageLog4j.java
@@ -8,10 +8,28 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        byte b = 1;
+        LOG.debug("debug message for byte var: {}", b);
+
+        short s = 2;
+        LOG.debug("debug message for byte var: {}", s);
+
+        int i = 3;
+        LOG.debug("debug message for byte var: {}", i);
+
+        long l = 4;
+        LOG.debug("debug message for byte var: {}", l);
+
+        float f = 5.1f;
+        LOG.debug("debug message for byte var: {}", f);
+
+        double d = 6.5;
+        LOG.debug("debug message for byte var: {}", d);
+
+        char c = 'a';
+        LOG.debug("debug message for byte var: {}", c);
+
+        boolean bool = true;
+        LOG.debug("debug message for byte var: {}", bool);
     }
 }
