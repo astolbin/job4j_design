bad26e5b7db07a5083f3fc18edbe139608b6971d
stolbin <stolbin@samsonpost.ru>
2021-03-31 07:50:53 +0300

    0. Видео [#345951]

---
+++ chapter_003/src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -0,0 +1,32 @@
+package ru.job4j.jdbc;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionDemo {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Class.forName("org.postgresql.Driver");
+
+        Settings settings = new Settings();
+        ClassLoader loader = Settings.class.getClassLoader();
+        try (InputStream io = loader.getResourceAsStream("app.properties")) {
+            settings.load(io);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String url = settings.getValue("db.url");
+        String login = settings.getValue("db.login");
+        String password = settings.getValue("db.password");
+
+        try (Connection connection = DriverManager.getConnection(url, login, password)) {
+            DatabaseMetaData metaData = connection.getMetaData();
+            System.out.println(metaData.getUserName());
+            System.out.println(metaData.getURL());
+        }
+    }
+}
---
+++ chapter_003/src/main/java/ru/job4j/jdbc/Settings.java
@@ -0,0 +1,20 @@
+package ru.job4j.jdbc;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Settings {
+    private final Properties prs = new Properties();
+
+    public void load(InputStream io) {
+        try {
+            this.prs.load(io);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getValue(String key) {
+        return this.prs.getProperty(key);
+    }
+}
